35a739b6d9f642f6bea9334fe56e33526f2d238b
sameer <sameerranjan048@gmail.com>
2025-05-28 22:32:23 +0530

    28/05/2025

--- app/src/main/java/com/example/adminwavesoffood/adapter/AddItemAdapter.kt
+++ app/src/main/java/com/example/adminwavesoffood/adapter/AddItemAdapter.kt
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.adminwavesoffood.databinding.ItemItemBinding
 
 class AddItemAdapter(private val MenuItemName:ArrayList<String>, private val MenuItemPrice:ArrayList<String>, private val MenuItemImage:ArrayList<Int>) : RecyclerView.Adapter<AddItemAdapter.AddItemViewHolder>(){
-
+private val itemQuantities = IntArray(MenuItemName.size){1}
 
     @SuppressLint("SuspiciousIndentation")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddItemViewHolder {
@@ -24,11 +24,46 @@ class AddItemAdapter(private val MenuItemName:ArrayList<String>, private val Men
     inner class AddItemViewHolder(private  val binding: ItemItemBinding) :RecyclerView.ViewHolder(binding.root) {
         fun bind(position: Int) {
           binding.apply {
+              val quantity =itemQuantities[position]
               fooddNameTextView.text= MenuItemName[position]
               textViewPrice.text= MenuItemPrice[position]
               foodImageview.setImageResource(MenuItemImage[position])
+              quantityTextView.text = quantity.toString()
+              minusButton.setOnClickListener{
+                  decreaseQuantity(position)
+
+              }
+              plusButton.setOnClickListener{
+                  increaseQuantity(position)
+
+              }
+              deleteButton.setOnClickListener{
+                  deleteQuantity(position)
+
+              }
 
           }
+        }
+
+        private fun increaseQuantity(position: Int) {
+            if(itemQuantities[position]<10){
+                itemQuantities[position]++
+                binding.quantityTextView.text = itemQuantities[position].toString()
+            }
+        }
+        private fun decreaseQuantity(position: Int) {
+            if(itemQuantities[position]>1){
+                itemQuantities[position]--
+                binding.quantityTextView.text = itemQuantities[position].toString()
+            }
+        }
+        private fun deleteQuantity(position: Int) {
+           MenuItemName.removeAt(position)
+            MenuItemPrice.removeAt(position)
+            MenuItemImage.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position,MenuItemName.size)
+
         }
 
     }
